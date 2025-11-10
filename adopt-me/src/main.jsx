8bd1b628288cbef60a5d021ddaa3a849698138ae
skperry99 <sperry939@gmail.com>
2025-11-10 15:21:05 -0600

    css import path update

--- adopt-me/src/main.jsx
+++ adopt-me/src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/globals.css"; // moved
+import "./globals.css"; // moved
 import App from "./app/App.jsx";
 
 createRoot(document.getElementById("root")).render(
