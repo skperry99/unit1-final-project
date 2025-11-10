6da1ca03f1bc053f8646428ae02262fa5968ce78
skperry99 <sperry939@gmail.com>
2025-11-10 13:34:38 -0600

    Added mobile menu toggle for Navbar

--- adopt-me/src/components/layout/Navbar.jsx
+++ adopt-me/src/components/layout/Navbar.jsx
@@ -1,38 +1,56 @@
-// src/components/layout/Navbar.jsx
 import { NavLink } from "react-router-dom";
-import { ROUTES } from "../../app/routes";
+import { useState } from "react";
 import logo from "../../assets/images/logo.jpg";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <nav className="navbar">
-      <NavLink to={ROUTES.HOME} end aria-label="Go to Home">
+      <NavLink to="/" end aria-label="Go to Home" onClick={closeMenu}>
         <img src={logo} id="navLogo" height="60" alt="BARK logo: cat and dog" />
       </NavLink>
 
-      <ul className="navbarList">
+      {/* Mobile toggle button */}
+      <button
+        className="navToggle"
+        aria-label="Toggle navigation menu"
+        aria-expanded={isOpen}
+        aria-controls="primary-navigation"
+        onClick={() => setIsOpen((v) => !v)}
+      >
+        <span className="navToggleBar" />
+        <span className="navToggleBar" />
+        <span className="navToggleBar" />
+      </button>
+
+      <ul
+        id="primary-navigation"
+        className={`navbarList ${isOpen ? "isOpen" : ""}`}
+      >
         <li>
-          <NavLink className="navListItem" to={ROUTES.HOME} end>
+          <NavLink className="navListItem" to="/" end onClick={closeMenu}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink className="navListItem" to={ROUTES.ABOUT}>
+          <NavLink className="navListItem" to="/about" onClick={closeMenu}>
             About
           </NavLink>
         </li>
         <li>
-          <NavLink className="navListItem" to={ROUTES.CONTACT}>
+          <NavLink className="navListItem" to="/contact" onClick={closeMenu}>
             Contact
           </NavLink>
         </li>
         <li>
-          <NavLink className="navListItem" to={ROUTES.ANIMALS}>
+          <NavLink className="navListItem" to="/animals" onClick={closeMenu}>
             Animals
           </NavLink>
         </li>
         <li>
-          <NavLink className="navListItem" to={ROUTES.STORIES}>
+          <NavLink className="navListItem" to="/stories" onClick={closeMenu}>
             Stories
           </NavLink>
         </li>
