29e8f4546c834cc91218ef38c579149d4bdf90f6
skperry99 <sperry939@gmail.com>
2025-11-10 13:09:58 -0600

    Updated main.jsx and added meta description to index.html

--- adopt-me/src/main.jsx
+++ adopt-me/src/main.jsx
@@ -1,7 +1,8 @@
+// src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./globals.css";
-import App from "./App.jsx";
+import "./styles/globals.css"; // moved
+import App from "./app/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
