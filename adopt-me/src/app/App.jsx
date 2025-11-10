43c5a681464b1a72ec3785c0263237442c094419
skperry99 <sperry939@gmail.com>
2025-11-10 12:57:31 -0600

    Adjusted folder structure, renamed css file and CareersPage, deleted App.css (empty)

--- adopt-me/src/app/App.jsx
+++ adopt-me/src/app/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
-import AboutPage from "./pages/AboutPage.jsx";
-import AnimalsPage from "./pages/AnimalsPage.jsx";
-import ContactPage from "./pages/ContactPage.jsx";
-import AdoptionFormPage from "./pages/AdoptionFormPage.jsx";
+import HomePage from "../pages/HomePage.jsx";
+import AboutPage from "../pages/AboutPage.jsx";
+import AnimalsPage from "../pages/AnimalsPage.jsx";
+import ContactPage from "../pages/ContactPage.jsx";
+import AdoptionFormPage from "../pages/AdoptionFormPage.jsx";
 import Careers from "./pages/Careers.jsx";
-import VolunteerPage from "./pages/VolunteerPage.jsx";
-import Stories from "./pages/StoriesPage.jsx";
-import Footer from "./components/Footer.jsx";
-import Navbar from "./components/Navbar.jsx";
-import Header from "./components/Header.jsx";
+import VolunteerPage from "../pages/VolunteerPage.jsx";
+import Stories from "../pages/StoriesPage.jsx";
+import Footer from "../components/layout/Footer.jsx";
+import Navbar from "../components/layout/Navbar.jsx";
+import Header from "../components/layout/Header.jsx";
 import banner from "./assets/images/banner-graphic.jpg";
 import "./App.css";
 
