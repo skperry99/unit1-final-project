e04f8eb5d541b415bb3e15029ef59ad0d53f968e
skperry99 <sperry939@gmail.com>
2025-11-10 13:11:25 -0600

    Added routes.jsx

---
+++ adopt-me/src/app/routes.jsx
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: "/",
+  ABOUT: "/about",
+  ANIMALS: "/animals",
+  CONTACT: "/contact",
+  ADOPTION: "/adoption",
+  CAREERS: "/careers",
+  VOLUNTEER: "/volunteer",
+  STORIES: "/stories",
+};
